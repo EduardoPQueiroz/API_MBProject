e6992e08440c0e76391da3608203a406721c32cb
Eduardo Pereira de Queiroz <eduardopq4@gmail.com>
2025-06-11 23:18:23 -0300

    feat: starting project, mapping classes, adding enums

---
+++ Models/Enums/TipoUsuario.cs
@@ -0,0 +1,8 @@
+namespace API_MBProject.Models.Enums
+{
+    public enum TipoUsuario
+    {
+        Musico,
+        Contratante
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_MBProject.Models.Enums;
+
+namespace API_MBProject.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string telefone { get; set; }
+        public string endereco { get; set; }
+        public byte[] fotoPerfil { get; set; }
+        public string biografia { get; set; }
+        public DateTime dtCriacao { get; set; }
+        public string links { get; set; }
+        public TipoUsuario tipoUsuario { get; set; }
+    }
+}
